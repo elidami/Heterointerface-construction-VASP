c30f290ded55ce680361ef75906ca5054e566918
elidami <elidami3399@gmail.com>
2023-06-08 16:08:34 +0200

    Implementation of a function that calculates the shift to apply to the upper slab in the z direction in order to have the optimal distance from the adatom and creation of the POSCAR file of the decorated interface. The shift in the xy plane is calculated so that the adatom is in the optimal adsorption site of the upper slab.

--- decorated_interface.py
+++ decorated_interface.py
@@ -52,12 +52,23 @@ def reflect_coord(coord_list, plane_point, plane_normal):
         reflected_coord.append(reflected_point)
     return reflected_coord
 
-def shift_coordinates(coord_list, shift_x, shift_y, shift_z):
+def shift_coordinates(coord_list, shift_z):
     shifted_coords = []
     for coord in coord_list:
-        shifted_coord = np.array([coord[0] + shift_x, coord[1] + shift_y, coord[2] + shift_z])
+        shifted_coord = np.array([coord[0], coord[1], coord[2] + shift_z])
         shifted_coords.append(shifted_coord)
     return shifted_coords
+
+def highest_z_value(coord):
+    coord_excluded_first = coord[1:]
+    # Ordina le coordinate in base al valore di z in modo crescente
+    sorted_coord = sorted(coord_excluded_first, key=lambda x: x[2])
+
+    # Trova il valore di z più alto
+    highest_z_value = sorted_coord[-1][2]
+
+    return highest_z_value
+
 
 
 
@@ -81,14 +92,22 @@ plane_point = np.array([0, 0,  z])
 plane_normal = np.array([0, 0, 1])
 reflected_coords = reflect_coord(cartesian_coord_upper_slab, plane_point, plane_normal)
 
-shift_x = 0
-shift_y = 0
-interlayer_distance_bottom_slab =1.545855346
-interlayer_distance_upper_slab =2.098396221
-slabs_distance = (interlayer_distance_bottom_slab+interlayer_distance_upper_slab)/2
 
-shift_z = cartesian_coord_bottom_slab[-1][2]+slabs_distance-cartesian_coord_upper_slab[-1][2]
-shifted_coords = shift_coordinates(reflected_coords,shift_x,shift_y,shift_z)
+lattice_vectors_adsorption = extract_lattice_vectors('adsorption_on_upper_slab.txt')
+a_adsorption = lattice_vectors_adsorption['a']
+b_adosrption = lattice_vectors_adsorption['b']
+c_adsorption = lattice_vectors_adsorption['c']
+
+# Extract atomic coordinates
+atomic_coord_adsorption = extract_atomic_coordinates('adsorption_on_upper_slab.txt')
+
+# Convert direct coordinates to Cartesian coordinates
+cartesian_coord_adsorption = direct_to_cartesian_coord(a_adsorption, b_adosrption, c_adsorption, atomic_coord_adsorption)
+adatom_slab_distance = cartesian_coord_adsorption[0][2]-highest_z_value(cartesian_coord_adsorption)
+shift_z = cartesian_coord_bottom_slab[0][2]+adatom_slab_distance-cartesian_coord_upper_slab[-1][2]
+shifted_coords = shift_coordinates(reflected_coords,shift_z)
+
+
 
 OutputFile = open("reflected_upper_slab.txt","w")
 with open('upper_slab.txt', 'r') as f:
@@ -97,14 +116,8 @@ with open('upper_slab.txt', 'r') as f:
     AtomTypeUpperSlab = upper_slab_lines[5].strip()
     AtomNumberUpperSlab = upper_slab_lines[6].strip()
 
-x_relax = True
-y_relax = True
-z_relax = True
-AtomCoordsUpperSlab = ["{}  {}  {}  {}\n".format('\t'.join(["{:<20.16f}".format(coord) for coord in coordinates.astype(float)]),
-                        "T" if x_relax else "F",
-                        "T" if y_relax else "F",
-                        "T" if z_relax else "T"
-                         ) for coordinates in shifted_coords]
+AtomCoordsUpperSlab = ["{}\n".format(' '.join(["{:<20.16f}".format(coord) for coord in coordinates.astype(float)])
+                        ) for coordinates in shifted_coords]
 HeaderAndScalingFactor = ["{}\n".format(HeaderUpperSlab),"1.0\n"]
 LatticeVectors = ["{}\n".format(' '.join(["{:<20.16f}".format(value) for value in vector])) for vector in [a, b, c]]
 AtomTypes = ["{}\n".format(AtomTypeUpperSlab)]
@@ -114,11 +127,11 @@ OutputFile.writelines(HeaderAndScalingFactor)
 OutputFile.writelines(LatticeVectors)
 OutputFile.writelines(AtomTypes)
 OutputFile.writelines(AtomNumbers)
-OutputFile.writelines("Selective Dynamics\nCartesian\n")
+OutputFile.writelines("Cartesian\n")
 OutputFile.writelines(AtomCoordsUpperSlab)
 OutputFile.close()
 
-def shit_for_adatom_adsorption_on_upper_slab(file_path, selected_site):
+def shift_for_adatom_adsorption_on_upper_slab(file_path, selected_site):
     original_file = file_path
     temporary_file = "POSCAR"
 
@@ -140,34 +153,85 @@ def shit_for_adatom_adsorption_on_upper_slab(file_path, selected_site):
 
     if selected_site == "top":
         reference_site = high_symmetry_sites[0][1]
-    elif selected_site == "hollow_fcc":
-        reference_site = high_symmetry_sites[1][1]
     elif selected_site == "hollow_hcp":
+        reference_site = high_symmetry_sites[1][1]
+    elif selected_site == "hollow_fcc":
         reference_site = high_symmetry_sites[2][1]
     else:
         print("Reference site not valid.")
         return
 
-    shift_x = -reference_site.coords[0]
-    shift_y = -reference_site.coords[1]
+    shift_x = cartesian_coord_bottom_slab[0][0]-reference_site.coords[0]
+    shift_y = cartesian_coord_bottom_slab[0][1]-reference_site.coords[1]
 
     # Shifting of the atomic coordinates in the POSCAR file
     for site in structure:
         site.coords[0] += shift_x
         site.coords[1] += shift_y
 
-    for site in structure:
-        print("Label:", site.species_string)
-        print("Coordinates:", site.coords)
-        print()
 
     # Original file name restoration
     os.rename(temporary_file, original_file)
+    # Restituisci la lista di coordinate shiftate
+    shifted_coords = [site.coords for site in structure]
+
+    return shifted_coords
 
 file_path = "reflected_upper_slab.txt"
 selected_site = "hollow_fcc"
 
-upper_slab_coords_for_adatom_adsorption = shit_for_adatom_adsorption_on_upper_slab(file_path, selected_site)
+upper_slab_coords_for_adatom_adsorption = shift_for_adatom_adsorption_on_upper_slab(file_path, selected_site)
+print(upper_slab_coords_for_adatom_adsorption)
+
+OutputFile = open("POSCAR","w")
+with open('upper_slab.txt', 'r') as f:
+    upper_slab_lines = f.readlines()
+    HeaderUpperSlab = upper_slab_lines[0].strip()
+    AtomTypeUpperSlab = upper_slab_lines[5].strip()
+    AtomNumberUpperSlab = upper_slab_lines[6].strip()
+
+
+with open('bottom_slab_with_adatom.txt', 'r') as f:
+    bottom_slab_lines = f.readlines()
+    HeaderBottomSlab = bottom_slab_lines[0].strip()
+    AtomTypeBottomSlab = bottom_slab_lines[5].strip()
+    AtomNumberBottomSlab = bottom_slab_lines[6].strip()
+
+
+
+def write_coords(coords, x_relax, y_relax, z_relax):
+    atom_coords = []
+    for coord in coords:
+        coord_string = "{}  {}  {}  {}\n".format(
+                    ' '.join(["{:<20.16f}".format(c) for c in coord.astype(float)]),
+                    "T" if x_relax else "F",
+                    "T" if y_relax else "F",
+                    "T" if z_relax else "F"
+        )
+        atom_coords.append(coord_string)
+    return atom_coords  
+
+x_relax = True
+y_relax = True
+z_relax = True
+
+AtomCoordsBottomSlab = write_coords(cartesian_coord_bottom_slab, x_relax, y_relax, z_relax)
+AtomCoordsUpperSlab = write_coords(upper_slab_coords_for_adatom_adsorption, x_relax, y_relax, z_relax)       
+
+HeaderAndScalingFactor = ["INTERFACE {}/{}\n".format(HeaderBottomSlab,HeaderUpperSlab),"1.0\n"]
+LatticeVectors = ["{}\n".format(' '.join(["{:<20.16f}".format(value) for value in vector])) for vector in [a, b, c]]
+AtomTypes = ["{} {}\n".format(AtomTypeBottomSlab,AtomTypeUpperSlab)]
+AtomNumbers = ["{} {}\n".format(AtomNumberBottomSlab,AtomNumberUpperSlab)]
+
+OutputFile.writelines(HeaderAndScalingFactor)
+OutputFile.writelines(LatticeVectors)
+OutputFile.writelines(AtomTypes)
+OutputFile.writelines(AtomNumbers)
+OutputFile.writelines("Selective Dynamics\nCartesian\n")
+OutputFile.writelines(AtomCoordsBottomSlab)
+OutputFile.writelines(AtomCoordsUpperSlab)
+OutputFile.close()
+
 
 
 
