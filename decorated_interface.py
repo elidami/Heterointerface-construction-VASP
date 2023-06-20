1736ffd12ebd692d328e5586dc211bc64b234891
elidami <elidami3399@gmail.com>
2023-06-20 16:28:49 +0200

    Deleted some comments and added the paths to the folders of the txt files

--- decorated_interface.py
+++ decorated_interface.py
@@ -17,27 +17,27 @@ def distance_between_highest_z_values(coord):
 
 
 # Extract lattice vectors
-lattice_vectors =  functions.extract_lattice_vectors('bottom_slab_with_adatom.txt')
+lattice_vectors =  functions.extract_lattice_vectors('decorated_interface_files/bottom_slab_with_adatom.txt')
 a = lattice_vectors['a']
 b = lattice_vectors['b']
 c = lattice_vectors['c']
 
 # Extract atomic coordinates
-atomic_coord_bottom_slab =  functions.extract_atomic_coordinates('bottom_slab_with_adatom.txt')
-atomic_coord_upper_slab =  functions.extract_atomic_coordinates('upper_slab.txt')
+atomic_coord_bottom_slab =  functions.extract_atomic_coordinates('decorated_interface_files/bottom_slab_with_adatom.txt')
+atomic_coord_upper_slab =  functions.extract_atomic_coordinates('decorated_interface_files/upper_slab.txt')
 
 # Convert direct coordinates to Cartesian coordinates
 cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
 cartesian_coord_upper_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_upper_slab)
 
 
-lattice_vectors_adsorption =  functions.extract_lattice_vectors('adsorption_on_upper_slab.txt')
+lattice_vectors_adsorption =  functions.extract_lattice_vectors('decorated_interface_files/adsorption_on_upper_slab.txt')
 a_adsorption = lattice_vectors_adsorption['a']
 b_adosrption = lattice_vectors_adsorption['b']
 c_adsorption = lattice_vectors_adsorption['c']
 
 # Extract atomic coordinates
-atomic_coord_adsorption =  functions.extract_atomic_coordinates('adsorption_on_upper_slab.txt')
+atomic_coord_adsorption =  functions.extract_atomic_coordinates('decorated_interface_files/adsorption_on_upper_slab.txt')
 
 
 def shift_for_adatom_adsorption_on_upper_slab(file_path, selected_site):
@@ -66,10 +66,10 @@ def shift_for_adatom_adsorption_on_upper_slab(file_path, selected_site):
 
     return shifted_coords
 
-file_path = "upper_slab.txt"
+file_path = "decorated_interface_files/upper_slab.txt"
 selected_site_Cu = "hollow_fcc"
 
-reference_site_Cu = functions.metal_fcc_111_high_symmetry_points("upper_slab.txt",selected_site_Cu)
+reference_site_Cu = functions.metal_fcc_111_high_symmetry_points("decorated_interface_files/upper_slab.txt",selected_site_Cu)
 upper_slab_coords_for_adatom_adsorption = shift_for_adatom_adsorption_on_upper_slab(file_path, reference_site_Cu)
 
 
@@ -85,40 +85,9 @@ shifted_coords =  functions.shift_slab_along_z(reflected_coords,shift_z)
 x_relax = True
 y_relax = True
 z_relax = True
-'''
-OutputFile = open("POSCAR","w")
-with open('upper_slab.txt', 'r') as f:
-    upper_slab_lines = f.readlines()
-    HeaderUpperSlab = upper_slab_lines[0].strip()
-    AtomTypeUpperSlab = upper_slab_lines[5].strip()
-    AtomNumberUpperSlab = upper_slab_lines[6].strip()
 
 
-with open('bottom_slab_with_adatom.txt', 'r') as f:
-    bottom_slab_lines = f.readlines()
-    HeaderBottomSlab = bottom_slab_lines[0].strip()
-    AtomTypeBottomSlab = bottom_slab_lines[5].strip()
-    AtomNumberBottomSlab = bottom_slab_lines[6].strip()
-
-
-AtomCoordsBottomSlab = functions.write_coords(cartesian_coord_bottom_slab, x_relax, y_relax, z_relax)
-AtomCoordsUpperSlab = functions.write_coords(shifted_coords, x_relax, y_relax, z_relax)       
-
-HeaderAndScalingFactor = ["INTERFACE {}/{}\n".format(HeaderBottomSlab,HeaderUpperSlab),"1.0\n"]
-LatticeVectors = ["{}\n".format(' '.join(["{:<20.16f}".format(value) for value in vector])) for vector in [a, b, c]]
-AtomTypes = ["{} {}\n".format(AtomTypeBottomSlab,AtomTypeUpperSlab)]
-AtomNumbers = ["{} {}\n".format(AtomNumberBottomSlab,AtomNumberUpperSlab)]
-
-OutputFile.writelines(HeaderAndScalingFactor)
-OutputFile.writelines(LatticeVectors)
-OutputFile.writelines(AtomTypes)
-OutputFile.writelines(AtomNumbers)
-OutputFile.writelines("Selective Dynamics\nCartesian\n")
-OutputFile.writelines(AtomCoordsBottomSlab)
-OutputFile.writelines(AtomCoordsUpperSlab)
-OutputFile.close() '''
-
-functions.write_POSCAR_interface('upper_slab.txt', 'bottom_slab_with_adatom.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
+functions.write_POSCAR_interface('decorated_interface_files/upper_slab.txt', 'decorated_interface_files/bottom_slab_with_adatom.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
 
 
 
--- interface.py
+++ interface.py
@@ -4,14 +4,14 @@ import os
 import functions
 
 # Extract lattice vectors
-lattice_vectors = functions.extract_lattice_vectors('bottom_slab.txt')
+lattice_vectors = functions.extract_lattice_vectors('clean_interface_files/bottom_slab.txt')
 a = lattice_vectors['a']
 b = lattice_vectors['b']
 c = lattice_vectors['c']
 
 # Extract atomic coordinates
-atomic_coord_bottom_slab = functions.extract_atomic_coordinates('bottom_slab.txt')
-atomic_coord_upper_slab =  functions.extract_atomic_coordinates('upper_slab.txt')
+atomic_coord_bottom_slab = functions.extract_atomic_coordinates('clean_interface_files/bottom_slab.txt')
+atomic_coord_upper_slab =  functions.extract_atomic_coordinates('clean_interface_files/upper_slab.txt')
 
 # Convert direct coordinates to Cartesian coordinates
 cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
@@ -40,12 +40,12 @@ def  shift_slab_on_xy(file_path, selected_site_Cu,selected_site_C):
     return shifted_coords
 
 selected_site_Cu = "top"
-selected_site_C = "top"
+selected_site_C = "hollow_fcc"
 
-reference_site_Cu = functions.metal_fcc_111_high_symmetry_points("upper_slab.txt",selected_site_Cu)
+reference_site_Cu = functions.metal_fcc_111_high_symmetry_points("clean_interface_files/upper_slab.txt",selected_site_Cu)
 print(reference_site_Cu.coords[0])
-reference_site_C = functions.C_111_high_symmetry_points("bottom_slab.txt", selected_site_C)
-shifted_upper_slab_on_xy = shift_slab_on_xy("upper_slab.txt", reference_site_Cu,reference_site_C)
+reference_site_C = functions.C_111_high_symmetry_points("clean_interface_files/bottom_slab.txt", selected_site_C)
+shifted_upper_slab_on_xy = shift_slab_on_xy("clean_interface_files/upper_slab.txt", reference_site_Cu,reference_site_C)
 
 z = cartesian_coord_upper_slab[-1][2] if cartesian_coord_upper_slab else None
 plane_point = np.array([0, 0,  z])
@@ -62,40 +62,6 @@ shifted_coords =  functions.shift_slab_along_z(reflected_coords,shift_z)
 x_relax = True
 y_relax = True
 z_relax = True
-'''
-OutputFile = open("POSCAR","w")
-with open('upper_slab.txt', 'r') as f:
-    upper_slab_lines = f.readlines()
-    HeaderUpperSlab = upper_slab_lines[0].strip()
-    AtomTypeUpperSlab = upper_slab_lines[5].strip()
-    AtomNumberUpperSlab = upper_slab_lines[6].strip()
 
-
-with open('bottom_slab.txt', 'r') as f:
-    bottom_slab_lines = f.readlines()
-    HeaderBottomSlab = bottom_slab_lines[0].strip()
-    AtomTypeBottomSlab = bottom_slab_lines[5].strip()
-    AtomNumberBottomSlab = bottom_slab_lines[6].strip()
-
- 
-
-AtomCoordsBottomSlab = functions.write_coords(cartesian_coord_bottom_slab, x_relax, y_relax, z_relax)
-AtomCoordsUpperSlab = functions.write_coords(shifted_coords, x_relax, y_relax, z_relax)       
-
-HeaderAndScalingFactor = ["INTERFACE {}/{}\n".format(HeaderBottomSlab,HeaderUpperSlab),"1.0\n"]
-LatticeVectors = ["{}\n".format(' '.join(["{:<20.16f}".format(value) for value in vector])) for vector in [a, b, c]]
-AtomTypes = ["{} {}\n".format(AtomTypeBottomSlab,AtomTypeUpperSlab)]
-AtomNumbers = ["{} {}\n".format(AtomNumberBottomSlab,AtomNumberUpperSlab)]
-
-OutputFile.writelines(HeaderAndScalingFactor)
-OutputFile.writelines(LatticeVectors)
-OutputFile.writelines(AtomTypes)
-OutputFile.writelines(AtomNumbers)
-OutputFile.writelines("Selective Dynamics\nCartesian\n")
-OutputFile.writelines(AtomCoordsBottomSlab)
-OutputFile.writelines(AtomCoordsUpperSlab)
-OutputFile.close()
-'''
-
-functions.write_POSCAR_interface('upper_slab.txt', 'bottom_slab.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
+functions.write_POSCAR_interface('clean_interface_files/upper_slab.txt', 'clean_interface_files/bottom_slab.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
 
