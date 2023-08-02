590b7984a239364b4d4f739b32bf0b9ff549d54e
elidami <elidami3399@gmail.com>
2023-08-02 17:59:49 +0200

    added comments to describe the following methods: extract_lattice_vectors, extract_atomic_coordinates, direct_to_cartesian_coord and reflect_coord

--- functions.py
+++ functions.py
@@ -5,26 +5,49 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 import os
 
 def extract_lattice_vectors(filename):
-    # Read the text file into a pandas DataFrame, skipping the header row
+    '''This method reads the text file (VASP POSCAR or CONTCAR file)
+       into a pandas DataFrame and extracts the lattice vectors.
+
+       Args:
+            filename: path to the text file 
+
+        Returns:
+            The lattice vectors a,b,c as a dictionary.'''
     df = pd.read_csv(filename, skiprows=lambda x: x not in [2, 3, 4], delim_whitespace=True, header=None, engine='python')
     
     a = np.array(df.iloc[0, 0:3]).astype(float)
     b = np.array(df.iloc[1, 0:3]).astype(float)
     c = np.array(df.iloc[2, 0:3]).astype(float)
 
-    # Return the extracted values as a dictionary
     return {'a': a, 'b': b, 'c': c}
 
 def extract_atomic_coordinates(filename):
+    '''This method reads the text file (VASP CONTCAR file)
+       into a pandas DataFrame and extracts the atomic coordinates.
+       
+       Args:
+            filename: path to the text file 
+       
+       Returns:
+            The atomic coordinates.'''
     df = pd.read_csv(filename, skiprows= 9, sep='\s+', header=None, engine='python')
     
-    last_row_index = int(len(df.index)/2)
+    last_row_index = int(len(df.index)/2) #In order to exclude the series of numbers 0 (initial velocities of atoms), division by 2 is necessary.
     coordinates_df = df.iloc[:last_row_index, 0:3]
     coordinates = coordinates_df.values.astype(float)
 
     return coordinates
   
 def direct_to_cartesian_coord(a,b,c, direct_coord):
+    '''This method converts direct atomic coordinates to cartesian
+       coordinates.
+    
+       Args:
+            a,b,c: the three lattice vectors of the cell
+            direct_coord: set of direct coordinates of the atoms
+        
+       Returns:
+            Cartesian coordinates of the atoms as a list of numpy arrays.'''
     cartesian_coords = []
     for coord in direct_coord:
         cartesian_coord = coord[0] * a + coord[1] * b + coord[2] * c
@@ -33,17 +56,16 @@ def direct_to_cartesian_coord(a,b,c, direct_coord):
     return cartesian_coords
 
 def reflect_coord(coord_list, plane_point, plane_normal):
-    """
-    Reflects a point across a plane defined by a point on the plane and its normal vector.
+    '''This method reflects a list of points across a plane, which is
+       defined by a point on the plane and its normal vector.
     
-    Args:
-        point: A numpy array of shape (3,) representing a point with x, y, z coordinates.
-        plane_point: A numpy array of shape (3,) representing a point on the plane with x, y, z coordinates.
-        plane_normal: A numpy array of shape (3,) representing the normal vector of the plane.
+        Args:
+            coord_list: list of numpy array of shape (3,) representing atomic coordinates.
+            plane_point: a numpy array of shape (3,) representing a point on the plane with x, y, z coordinates.
+            plane_normal: a numpy array of shape (3,) representing the normal vector of the plane.
         
-    Returns:
-        A numpy array of shape (3,) representing the reflected point.
-    """
+        Returns:
+            List of numpy arrays of shape (3,) representing the reflected points.'''
     reflected_coord = []
     for coord in coord_list:
         v = coord - plane_point
