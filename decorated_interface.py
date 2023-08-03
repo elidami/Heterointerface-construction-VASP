0bcbf205d9e495a9a500a20a1bc8b3bd4a420b71
elidami <elidami3399@gmail.com>
2023-08-03 19:30:17 +0200

    Add comments to explain the code. Modified the parts in which a specific coordinate was given through its index, with a string that sorts the coordinates in ascending order of z and takes the coordinates corresponding to the highest z value. In this way the code is more general and is able to handle files in which the coordinates are written in random order.

--- decorated_interface.py
+++ decorated_interface.py
@@ -6,94 +6,45 @@ import configparser
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
-# Extract lattice vectors
+# Extract lattice vectors for interface supercell
 lattice_vectors =  functions.extract_lattice_vectors('decorated_interface_files/bottom_slab_with_adatom.txt')
 a = lattice_vectors['a']
 b = lattice_vectors['b']
 c = lattice_vectors['c']
 
-# Extract atomic coordinates
-atomic_coord_bottom_slab =  functions.extract_atomic_coordinates('decorated_interface_files/bottom_slab_with_adatom.txt')
-atomic_coord_upper_slab =  functions.extract_atomic_coordinates('decorated_interface_files/upper_slab.txt')
-
-# Convert direct coordinates to Cartesian coordinates
-cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
-cartesian_coord_upper_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_upper_slab)
-
-
+# Extract lattice vectors for supercell that regards adatom adsorption on upper slab
 lattice_vectors_adsorption =  functions.extract_lattice_vectors('decorated_interface_files/adsorption_on_upper_slab.txt')
 a_adsorption = lattice_vectors_adsorption['a']
 b_adosrption = lattice_vectors_adsorption['b']
 c_adsorption = lattice_vectors_adsorption['c']
 
-# Extract atomic coordinates
+# Extract atomic coordinates of bottom and upper slabs
+atomic_coord_bottom_slab =  functions.extract_atomic_coordinates('decorated_interface_files/bottom_slab_with_adatom.txt')
+atomic_coord_upper_slab =  functions.extract_atomic_coordinates('decorated_interface_files/upper_slab.txt')
 atomic_coord_adsorption =  functions.extract_atomic_coordinates('decorated_interface_files/adsorption_on_upper_slab.txt')
 
-file_path = "decorated_interface_files/upper_slab.txt"
+# Convert direct coordinates to Cartesian coordinates
+cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
+cartesian_coord_upper_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_upper_slab)
+cartesian_coord_adsorption =  functions.direct_to_cartesian_coord(a_adsorption, b_adosrption, c_adsorption, atomic_coord_adsorption)
+
+# Shift upper slab on xy plane so that its selected high symmetry point is aligned with the adatom adsorbed on bottom slab
 selected_site_Cu = config.get('settings', 'selected_site_Cu')
-
 reference_site_Cu = functions.metal_fcc_111_high_symmetry_points("decorated_interface_files/upper_slab.txt",selected_site_Cu)
-print(reference_site_Cu)
-upper_slab_coords_for_adatom_adsorption = functions.shift_slab_on_xy(file_path, reference_site_Cu,cartesian_coord_bottom_slab[0])
+upper_slab_coords_for_adatom_adsorption = functions.shift_slab_on_xy("decorated_interface_files/upper_slab.txt", reference_site_Cu,max(cartesian_coord_bottom_slab, key=lambda x: x[2]))
 
-
-z = upper_slab_coords_for_adatom_adsorption[-1][2] if upper_slab_coords_for_adatom_adsorption else None
+# Reflect atomic coordinates of the upper slab with respect to its highest atom along z direction
+z = max(cartesian_coord_upper_slab, key=lambda x: x[2])[2]
 plane_point = np.array([0, 0,  z])
 plane_normal = np.array([0, 0, 1])
 reflected_coords =  functions.reflect_coord(upper_slab_coords_for_adatom_adsorption, plane_point, plane_normal)
-# Convert direct coordinates to Cartesian coordinates
-cartesian_coord_adsorption =  functions.direct_to_cartesian_coord(a_adsorption, b_adosrption, c_adsorption, atomic_coord_adsorption)
-shift_z = cartesian_coord_bottom_slab[0][2]+functions.distance_between_highest_z_values(cartesian_coord_adsorption)-cartesian_coord_upper_slab[-1][2]
+
+# The distance between the two slabs is determined by the distance of the adatom when it is adsorbed on the upper slab  
+shift_z = max(cartesian_coord_bottom_slab, key=lambda x: x[2])[2]+functions.distance_between_highest_z_values(cartesian_coord_adsorption)-max(cartesian_coord_upper_slab, key=lambda x: x[2])[2]
 shifted_coords =  functions.shift_slab_along_z(reflected_coords,shift_z)
 
 x_relax = config.get('settings', 'x_relax')
 y_relax = config.get('settings', 'y_relax')
 z_relax = config.get('settings', 'z_relax')
 
-
 functions.write_POSCAR_interface('decorated_interface_files/upper_slab.txt', 'decorated_interface_files/bottom_slab_with_adatom.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
