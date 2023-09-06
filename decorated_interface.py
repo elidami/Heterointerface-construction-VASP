cb66ad3952b98e2333c958a7a49a4bd531265e03
elidami <elidami3399@gmail.com>
2023-09-06 19:25:30 +0200

    add to the configuration file the paths to the input txt files required from the script. Consequent implementation of this option in interface.py and decorated_interface.py scripts.

--- decorated_interface.py
+++ decorated_interface.py
@@ -6,22 +6,26 @@ import configparser
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
+bottom_slab_with_adatom = config.get('paths', 'bottom_slab_with_adatom')
+upper_slab = config.get('paths', 'decorated_upper_slab')
+adsorption_on_upper_slab = config.get('paths', 'adsorption_on_upper_slab')
+
 # Extract lattice vectors for interface supercell
-lattice_vectors =  functions.extract_lattice_vectors('decorated_interface_files/bottom_slab_with_adatom.txt')
+lattice_vectors =  functions.extract_lattice_vectors(bottom_slab_with_adatom)
 a = lattice_vectors['a']
 b = lattice_vectors['b']
 c = lattice_vectors['c']
 
 # Extract lattice vectors for supercell that regards adatom adsorption on upper slab
-lattice_vectors_adsorption =  functions.extract_lattice_vectors('decorated_interface_files/adsorption_on_upper_slab.txt')
+lattice_vectors_adsorption =  functions.extract_lattice_vectors(adsorption_on_upper_slab)
 a_adsorption = lattice_vectors_adsorption['a']
 b_adosrption = lattice_vectors_adsorption['b']
 c_adsorption = lattice_vectors_adsorption['c']
 
 # Extract atomic coordinates of bottom and upper slabs
-atomic_coord_bottom_slab =  functions.extract_atomic_coordinates('decorated_interface_files/bottom_slab_with_adatom.txt')
-atomic_coord_upper_slab =  functions.extract_atomic_coordinates('decorated_interface_files/upper_slab.txt')
-atomic_coord_adsorption =  functions.extract_atomic_coordinates('decorated_interface_files/adsorption_on_upper_slab.txt')
+atomic_coord_bottom_slab =  functions.extract_atomic_coordinates(bottom_slab_with_adatom)
+atomic_coord_upper_slab =  functions.extract_atomic_coordinates(upper_slab)
+atomic_coord_adsorption =  functions.extract_atomic_coordinates(adsorption_on_upper_slab)
 
 # Convert direct coordinates to Cartesian coordinates
 cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
@@ -30,8 +34,8 @@ cartesian_coord_adsorption =  functions.direct_to_cartesian_coord(a_adsorption, 
 
 # Shift upper slab on xy plane so that its selected high symmetry point is aligned with the adatom adsorbed on bottom slab
 selected_site_metal = config.get('settings', 'selected_site_metal')
-reference_site_metal = functions.metal_fcc_111_high_symmetry_points("decorated_interface_files/upper_slab.txt",selected_site_metal)
-upper_slab_coords_for_adatom_adsorption = functions.shift_slab_on_xy("decorated_interface_files/upper_slab.txt", reference_site_metal,max(cartesian_coord_bottom_slab, key=lambda x: x[2]))
+reference_site_metal = functions.metal_fcc_111_high_symmetry_points(upper_slab,selected_site_metal)
+upper_slab_coords_for_adatom_adsorption = functions.shift_slab_on_xy(upper_slab, reference_site_metal,max(cartesian_coord_bottom_slab, key=lambda x: x[2]))
 
 # Reflect atomic coordinates of the upper slab with respect to its highest atom along z direction
 z = max(cartesian_coord_upper_slab, key=lambda x: x[2])[2]
@@ -47,4 +51,4 @@ x_relax = config.get('settings', 'x_relax')
 y_relax = config.get('settings', 'y_relax')
 z_relax = config.get('settings', 'z_relax')
 
-functions.write_POSCAR_interface('decorated_interface_files/upper_slab.txt', 'decorated_interface_files/bottom_slab_with_adatom.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
+functions.write_POSCAR_interface(upper_slab, bottom_slab_with_adatom, cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
--- interface.py
+++ interface.py
@@ -6,15 +6,18 @@ import configparser
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
 
+bottom_slab= config.get('paths', 'clean_bottom_slab')
+upper_slab = config.get('paths', 'clean_upper_slab')
+
 # Extract lattice vectors
-lattice_vectors = functions.extract_lattice_vectors('clean_interface_files/bottom_slab.txt')
+lattice_vectors = functions.extract_lattice_vectors(bottom_slab)
 a = lattice_vectors['a']
 b = lattice_vectors['b']
 c = lattice_vectors['c']
 
 # Extract atomic coordinates of bottom and upper slabs
-atomic_coord_bottom_slab = functions.extract_atomic_coordinates('clean_interface_files/bottom_slab.txt')
-atomic_coord_upper_slab =  functions.extract_atomic_coordinates('clean_interface_files/upper_slab.txt')
+atomic_coord_bottom_slab = functions.extract_atomic_coordinates(bottom_slab)
+atomic_coord_upper_slab =  functions.extract_atomic_coordinates(upper_slab)
 
 # Convert direct coordinates to Cartesian coordinates
 cartesian_coord_bottom_slab =  functions.direct_to_cartesian_coord(a, b, c, atomic_coord_bottom_slab)
@@ -23,9 +26,9 @@ cartesian_coord_upper_slab =  functions.direct_to_cartesian_coord(a, b, c, atomi
 # Shift upper slab on xy plane so that the selected high symmetry points of upper and bottom slabs are aligned
 selected_site_metal = config.get('settings', 'selected_site_metal')
 selected_site_C = config.get('settings', 'selected_site_C')
-reference_site_metal = functions.metal_fcc_111_high_symmetry_points("clean_interface_files/upper_slab.txt",selected_site_metal)
-reference_site_C = functions.C_111_high_symmetry_points("clean_interface_files/bottom_slab.txt", selected_site_C)
-shifted_upper_slab_on_xy = functions.shift_slab_on_xy("clean_interface_files/upper_slab.txt", reference_site_metal,reference_site_C)
+reference_site_metal = functions.metal_fcc_111_high_symmetry_points(upper_slab,selected_site_metal)
+reference_site_C = functions.C_111_high_symmetry_points(bottom_slab, selected_site_C)
+shifted_upper_slab_on_xy = functions.shift_slab_on_xy(upper_slab, reference_site_metal,reference_site_C)
 
 # Reflect atomic coordinates of the upper slab with respect to its highest atom along z direction
 z = max(cartesian_coord_upper_slab, key=lambda x: x[2])[2]
@@ -44,5 +47,5 @@ x_relax = config.get('settings', 'x_relax')
 y_relax = config.get('settings', 'y_relax')
 z_relax = config.get('settings', 'z_relax')
 
-functions.write_POSCAR_interface('clean_interface_files/upper_slab.txt', 'clean_interface_files/bottom_slab.txt', cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
+functions.write_POSCAR_interface(upper_slab, bottom_slab, cartesian_coord_bottom_slab, x_relax, y_relax, z_relax, shifted_coords, a, b, c)
 
