13e885f99763850450525954b4c6541aa4173dbb
elidami <elidami3399@gmail.com>
2023-09-09 15:22:50 +0200

    Fixed the documentation.

--- Tests/test_reflection.py
+++ Tests/test_reflection.py
@@ -2,8 +2,27 @@ import functions
 import numpy as np
 import unittest
 
+'''
+    In this file are defined the classes PlaneParameters and TestReflectCoords
+    that contain the test methods for the functions that deal with reflection operations. '''
+
+
 class PlaneParameters(unittest.TestCase):
+    '''This class represents a unit test for the "calculate_plane_parameters" function. Different cases
+       are investigated in each of the following methods.
+   
+        Methods:
+             -test_z_coord_equal_zero: case with all z coordinates equal to zero.
+             -test_reflect_coord_wrt_y_axis: reflection wrt plane perpendicular to x direction that passes from the origin.
+             -test_invert_normals: verify that changing sign in plane normal coordinates don't change the results. '''
     def test_z_coord_equal_zero(self):
+        '''Test to verify that the plane point has z coordinate equal to zero when the list of all the z atomic coordinates
+           are equal to zero.
+           
+             Test Steps:
+                 -Define a list of three numpy arrays of shape (3,) representing atomic positions with the z coordinates equal to zero.
+                 -Execute the "calculate_plane_parameters" function, passing the coordinate list.
+                 -Verify if the returned results match the expected plane point.'''
         cartesian_coord_xy = [
             [1.0, 2.0, 0.0],
             [3.0, 4.0, 0.0],
@@ -11,11 +30,15 @@ class PlaneParameters(unittest.TestCase):
         ]
         plane_point, plane_normal = functions.calculate_plane_parameters(cartesian_coord_xy)
         expected_plane_point = np.array([0, 0, 0]) 
-        expected_plane_normal = np.array([0, 0, 1])
         assert np.array_equal(plane_point, expected_plane_point), "Test failed: plane_point doesn't match the expected result."
-        assert np.array_equal(plane_normal, expected_plane_normal), "Test failed: plane_normal doesn't match the expected result."
 
     def test_x_coord_equal_zero(self):
+        '''Test to verify that the plane_point variable is not affected by the fact that x atomic coordinates are equal to zero.
+        
+          Test Steps:
+              -Define a list of three numpy arrays of shape (3,) representing atomic positions with the z coordinates equal to zero.
+              -Execute the "calculate_plane_parameters" function, passing the coordinate list.
+              -Verify if the returned results match the expected plane point.'''
         cartesian_coord_yz = [
             [0.0, 1.0, 2.0],
             [0.0, 3.0, 4.0],
@@ -23,11 +46,15 @@ class PlaneParameters(unittest.TestCase):
         ]
         plane_point, plane_normal = functions.calculate_plane_parameters(cartesian_coord_yz)
         expected_plane_point = np.array([0, 0, 6.0])
-        expected_plane_normal = np.array([0, 0, 1]) 
         assert np.array_equal(plane_point, expected_plane_point), "Test failed: plane_point doesn't match the expected result."
-        assert np.array_equal(plane_normal, expected_plane_normal), "Test failed: plane_normal doesn't match the expected result."
 
     def test_general_case(self):
+        '''Test for a general case in which atomic coordinates are different from zero.
+
+        Test Steps:
+            -Define a list of three numpy arrays of shape (3,) representing random atomic positions.
+            -Execute the "calculate_plane_parameters" function, passing the coordinate list.
+            -Verify if the returned results match the expected plane point.'''
         cartesian_coord_variable_z = [
             [1.0, 2.0, 5.0],
             [3.0, 4.0, 8.0],
@@ -35,10 +62,8 @@ class PlaneParameters(unittest.TestCase):
             [7.0, 8.0, 7.0],
         ]
         plane_point, plane_normal = functions.calculate_plane_parameters(cartesian_coord_variable_z)
-        expected_plane_point = np.array([0, 0, 8.0]) 
-        expected_plane_normal = np.array([0, 0, 1])  
+        expected_plane_point = np.array([0, 0, 8.0])  
         assert np.array_equal(plane_point, expected_plane_point), "Test failed: plane_point doesn't match the expected result."
-        assert np.array_equal(plane_normal, expected_plane_normal), "Test failed: plane_normal doesn't match the expected result."
    
 
 class TestReflectCoords(unittest.TestCase):
@@ -48,9 +73,9 @@ class TestReflectCoords(unittest.TestCase):
             -coord_list: list of three numpy arrays of shape (3,) representing atomic positions.
             
        Methods:
-            -test_reflect_coord_wrt_x_axis
-            -test_reflect_coord_wrt_y_axis
-            -test_reflect_coord_wrt_y_axis'''
+            -test_reflect_coord_wrt_x_axis: reflection wrt plane perpendicular to x direction that passes from the origin.
+            -test_reflect_coord_wrt_y_axis: reflection wrt plane perpendicular to x direction that passes from the origin.
+            -test_invert_normals: verify that changing sign in plane normal coordinates don't change the results. '''
     coord_list = [
     np.array([1.0, 2.0, 3.0]),
     np.array([4.0, 5.0, 6.0]),
